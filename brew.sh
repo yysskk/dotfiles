2e0a0f793b4f263a0b5d3d5417e10dbd9b6c2cad
Yusuke Morishia <yysskk@yysskk.local>
2017-03-29 22:53:27 +0900

    update

---
+++ brew.sh
@@ -0,0 +1,24 @@
+brew update
+brew upgrade
+
+brew install ag
+brew install autoconf
+brew install automake
+brew install direnv
+brew install docker
+brew install docker-machine
+brew install ffmpeg
+brew install ffmpeg --with-libvpx
+brew install fontforge
+brew install git
+brew install go
+brew install hugo
+brew install highlight
+brew install imagemagick --with-webp
+brew install vim
+brew install wget
+brew install zopfli
+brew install zsh
+
+brew cleanup
+
---
+++ setup.sh
@@ -0,0 +1,34 @@
+# install homebrew
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew tap Homebrew/bundle
+sh brew.sh
+
+# set installed zsh & fish
+echo "/usr/local/bin/zsh" >> /etc/shells
+echo "/usr/local/bin/fish" >> /etc/shells
+chsh -s /usr/local/bin/zsh
+
+# set up antigen
+git clone https://github.com/zsh-users/antigen.git ~/.antigen
+git clone https://github.com/milkbikis/powerline-shell ~/.powerline-shell
+cd ~/.powerline-shell/;./install.sh
+
+# install nodenv, rbenv & ruby-build, pyenv, plenv & perl-build
+git clone https://github.com/nodenv/nodenv.git ~/.nodenv
+git clone https://github.com/nodenv/node-build.git ~/.nodenv/plugins/node-build
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+
+# symlinks
+if [ -f ~/.zshrc ]; then
+  rm ~/.zshrc
+fi
+ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/.zprofile ~/.zprofile
+ln -s ~/dotfiles/.exports ~/.exports
+ln -s ~/dotfiles/.aliases ~/.aliases
+ln -s ~/dotfiles/.functions ~/.functions
+ln -s ~/dotfiles/.gemrc ~/.gemrc
+ln -s ~/dotfiles/.vimrc ~/.vimrc
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/.editorconfig ~/.editorconfig
