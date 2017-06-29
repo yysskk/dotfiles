63fd7e0b76b4a30dc8736da29b635c2ac97b550f
yysskk <yusuke.0213@gmail.com>
2017-06-29 12:19:14 +0900

    add brew-cask setting

--- brew.sh
+++ brew.sh
@@ -22,3 +22,22 @@ brew install zsh
 
 brew cleanup
 
+## cask introduce
+brew tap caskroom/cask
+brew cask install alfred
+brew cask install atom
+brew cask install dropbox
+brew cask install google-japanese-ime
+brew cask install iterm2
+brew cask install sketch
+brew cask install slack
+brew cask install unity
+brew cask install xccello
+brew cask install dash
+brew cask install filezilla
+brew cask install hammerspoon
+brew cask install sketch-toolbox
+brew cask install spotify
+brew cask install xquartz
+brew cask install wireshark
+
