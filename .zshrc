697e3c0a2d95f30273073087cea37de08dc40257
yysskk <yusuke.0213@gmail.com>
2019-08-09 15:06:48 +0900

    Update

--- .zshrc
+++ .zshrc
@@ -34,3 +34,7 @@ export PATH=$HOME/cmdshelf/.build/release:$PATH
 # Flutter
 export PATH=$HOME/flutter/bin:$PATH
 
+export PATH="/usr/local/opt/llvm@6/bin:$PATH"
+export PATH="/usr/local/sbin:$PATH"
+export PATH=$PATH:/usr/local/bin
+
--- brew.sh
+++ brew.sh
@@ -5,11 +5,6 @@ brew install ag
 brew install autoconf
 brew install automake
 brew install direnv
-brew install docker
-brew install docker-machine
-brew install ffmpeg
-brew install ffmpeg --with-libvpx
-brew install fontforge
 brew install gibo
 brew install git-lfs
 brew install git
@@ -27,19 +22,11 @@ brew cleanup
 ## cask introduce
 brew tap caskroom/cask
 brew cask install alfred
-brew cask install atom
 brew cask install dropbox
 brew cask install google-japanese-ime
 brew cask install iterm2
 brew cask install sketch
 brew cask install slack
-brew cask install unity
-brew cask install xccello
-brew cask install dash
-brew cask install filezilla
 brew cask install hammerspoon
-brew cask install sketch-toolbox
 brew cask install spotify
-brew cask install xquartz
-brew cask install wireshark
 
